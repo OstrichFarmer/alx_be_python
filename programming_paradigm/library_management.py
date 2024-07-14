8113c0a91937cde2518e1bf321229a7c927d7b29
Joel Stephen <joel.stephen56.sj@gmail.com>
2024-07-14 12:19:22 +0100

    feat/ implemented the library management

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,52 @@
+class Book:
+    """Book class"""
+
+    def __init__(self, title, author):
+        """Initialize Book class
+
+        Args:
+            title (str): Title of the book
+            author (str): Author of the book
+        """
+        self.title = title
+        self.author = author
+        self.__is_checked_out = False
+
+    def is_checked_out(self):
+        """Check if a book is checked out"""
+        return self.__is_checked_out
+
+    def set_status(self, status):
+        """Set the status of a book"""
+        self.__is_checked_out = status
+
+
+class Library:
+    """Library class"""
+
+    def __init__(self):
+        """Initialize Library class"""
+        self.__books = []
+
+    def add_book(self, book):
+        """Add a book to the library"""
+        self.__books.append(book)
+
+    def check_out_book(self, title):
+        """Check out a book from the library"""
+        for book in self.__books:
+            if book.title == title:
+                book.set_status(True)
+
+    def return_book(self, title):
+        """Return a book to the library"""
+        for book in self.__books:
+            if book.title == title:
+                book.set_status(False)
+
+    def list_available_books(self):
+        """Display available books in the library"""
+        available_books = [
+            book for book in self.__books if not book.is_checked_out()]
+        for book in available_books:
+            print(f'{book.title} by {book.author}')
